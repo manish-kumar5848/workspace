3a615114facbd91bd0aae7e67ffc37d45a8d8127
Manish Kumar Yadav <59701564+manish-kumar5848@users.noreply.github.com>
2021-12-07 10:05:54 +0530

    Update app.js

--- jsWork/app.js
+++ jsWork/app.js
@@ -73,4 +73,47 @@ console.log(chessStory.match(/\W/));
 console.log("#############################");
 //exec() method executes a search for a match in a specified string. Returns a result array, or null.
 
-console.log("ARRAY")
+console.log("**************ARRAY***************8");
+let numbers = [23, 45, 65, 89, 87, 57, 64, 12, 54, 19];
+console.log(numbers);
+
+//looping
+
+// numbers.forEach((v)=>console.log(v*v))
+
+console.log(numbers.push(31)); //return index
+
+console.log(numbers.indexOf(65));
+
+// remove an element by its index position from the array
+let position = 2; // it defines from which index item to be removed
+let n = 1; // it defines how much item you want to remove
+let removedItem = numbers.splice(position, n);
+console.log(removedItem);
+
+console.log(numbers.toString());
+
+console.log("Length of an Array is " + numbers.length);
+let newSubarray = numbers.slice(1, 6);
+console.log(newSubarray);
+
+console.log(numbers.slice()); //if does not specify the start and end index in slice method it will return the whole array
+
+// console.log(numbers.isArray()); //
+let result =numbers.filter(number => number > 50);
+console.log(result);
+
+// let r2 = numbers.flat(5)
+// console.log(r2);   //The flat() method creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+
+let m1= numbers.map(number => number**3);
+console.log(m1);
+
+// let sum1=0;
+let reducer = (sum1,value)=>sum1+value;
+
+console.log(numbers.reduce(reducer,0));  // 3 is initial value;
+let r2 =numbers.sort();
+
+console.log(r2);
+
